78afa129d04266a022c8ca5509c8f35099f6cff5
Guilherme Ferreira <guifrribeiro@gmail.com>
2025-02-08 05:05:28 -0300

    Adding login endpoint.

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -4,5 +4,6 @@ import { AuthController } from "../src/controllers/auth.controller";
 const authRoutes = Router();
 
 authRoutes.post("/register", AuthController.register);
+authRoutes.post("/login", AuthController.login);
 
 export { authRoutes };
