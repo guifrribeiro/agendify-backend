3c59adddb2f807ec2d7a128c8f5fbe3499431bd2
Guilherme Ferreira <guifrribeiro@gmail.com>
2025-02-15 01:42:55 -0300

    Deleting appointment endpoints from auth routes.

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "../src/controllers/auth.controller";
+import { AppointmentController } from "../src/controllers/appointment.controller";
+import { ensureAuthenticated } from "../src/middlewares/auth.middleware";
 
 const authRoutes = Router();
+const appointmentRoutes = Router();
 
 authRoutes.post("/register", AuthController.register);
 authRoutes.post("/login", AuthController.login);
+
+
 
 export { authRoutes };
